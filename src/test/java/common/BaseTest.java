446bd982cb47e82aa3e24782ec8645f7a80a9906
Julia Guimaraes <yasminjullia@live.com>
2020-11-16 17:13:43 -0300

    adding allure reports puglins & finishing course
    Signed-off-by: Julia Guimaraes <yasminjullia@live.com>

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -2,9 +2,18 @@ package common;
 
 import com.codeborne.selenide.Configuration;
 import components.SideBar;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
 import pages.MoviePage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class BaseTest {
 
@@ -20,5 +29,21 @@ public class BaseTest {
         login = new LoginPage();
         side = new SideBar();
         movie = new MoviePage();
+    }
+
+    @AfterMethod
+    public void finish() {
+        String tempshot = screenshot("temp_shot");
+        try {
+            BufferedImage bimg = ImageIO.read(new File(tempshot));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bimg, "png", baos);
+            byte[] finalshot = baos.toByteArray();
+            io.qameta.allure.Allure.addAttachment("Evidencia", new ByteArrayInputStream(finalshot));
+
+        } catch (Exception e) {
+            System.out.println("Deu erro ao anexar o screenshot: " + e.getMessage());
+        }
+
     }
 }
